94007f235046fecbfc5e5b7e80eb0f2ed74c6ee9
c0d1ngd0j0 <sam.gdouglas@gmail.com>
2019-02-23 06:56:50 -0500

    folder structure setup, setup middlewares

--- server.js
+++ server.js
@@ -6,14 +6,20 @@ const PORT = (process.env.PORT || 3000);
 const app = express();
 
 // Middleware
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 // Database Connection
-
+require('./app/Database');
 // Models
+require('./app/Models/post');
 
 // Routes
+app.use('/api', require('./app/Routes/post'));
 
 // Error Handling
+
 
 // Initialize Server
 app.listen(PORT, (err) =>{
