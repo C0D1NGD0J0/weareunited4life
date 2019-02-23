3c909898bab5221b2b7dfbfc06ec362ed323eee9
c0d1ngd0j0 <sam.gdouglas@gmail.com>
2019-02-23 16:23:10 -0500

    refactored code in auth controller to use async/await

--- app/Controllers/authController.js
+++ app/Controllers/authController.js
@@ -6,33 +6,32 @@ const keys = require("../Config/keys");
 const validate = require("../Util/validations");
 
 const authCntrl = {
-	register: (req, res, next) =>{
+	register: async (req, res, next) =>{
 		const { errors, isValid } = validate.registration(req.body);
 		
 		if(!isValid){
 			return res.status(400).json(errors);
 		};
 
-		User.findOne({ email: req.body.email }).then(user =>{
-			if(user){
-				errors.email = "Email already exists!";
-				return res.status(400).json(errors);
-			} else {
-				const newUser = new User({
-					email: req.body.email,
-					username: req.body.username,
-					password: req.body.password
-				});
+		const existingUser = await User.findOne({ email: req.body.email });
+		if(existingUser){
+			errors.email = "Email already taken!";
+			return res.status(403).json(errors);
+		};
+		
+		const newUser = await new User({
+			email: req.body.email,
+			username: req.body.username,
+			password: req.body.password
+		});
 
-				bcrypt.genSalt(10, (err, salt) =>{
-					bcrypt.hash(newUser.password, salt, (err, hash) =>{
-						if(err) throw err;
-						newUser.password = hash;
-						newUser.save().then(user => res.json(user)).catch(err => console.log(err));
-					});
-				});
-			}
-		})
+		bcrypt.genSalt(10, (err, salt) =>{
+			bcrypt.hash(newUser.password, salt, (err, hash) =>{
+				if(err) throw err;
+				newUser.password = hash;
+				newUser.save().then(user => res.json(user)).catch(err => console.log(err));
+			});
+		});
 	},
 
 	login: (req, res, next) =>{
