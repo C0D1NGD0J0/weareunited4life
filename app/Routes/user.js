95698f85e5fdbfffa390440847ff34cc4ca542e4
c0d1ngd0j0 <sam.gdouglas@gmail.com>
2019-03-02 09:59:19 -0500

    unfollow route added

--- app/Routes/user.js
+++ app/Routes/user.js
@@ -6,10 +6,12 @@ const userCntrl = require("../Controllers/userController");
 
 router.get("/", passport.authenticate('jwt', {session: false}), userCntrl.index);
 
-router.put("/follow", passport.authenticate('jwt', {session: false}), userCntrl.follow);
+router.put("/:followId/follow", passport.authenticate('jwt', {session: false}), userCntrl.follow);
 
 router.get("/currentuser", passport.authenticate('jwt', {session: false}), userCntrl.currentuser);
 
 router.get("/:userId", passport.authenticate('jwt', {session: false}), userCntrl.profile);
+
+router.delete("/:followId/unfollow", passport.authenticate('jwt', {session: false}), userCntrl.follow);
 
 module.exports = router;
