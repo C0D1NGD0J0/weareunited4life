89b5aea559daeea54fdc322fde0b574c91c87007
c0d1ngd0j0 <sam.gdouglas@gmail.com>
2019-03-03 08:14:00 -0500

    register component updated with local state

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
-	
+	constructor(){
+		super();
+		this.state = {
+			email: '',
+			password: ''
+		}
+	}
+
 	render() {
 		return (
 			<div role="tabpanel" className="tab-pane active" id="login">
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,40 +1,117 @@
 import React, { Component } from 'react';
 
 class Register extends Component {
+	constructor(){
+		super();
+		this.state = {
+			email: '',
+			username: '',
+			location: '',
+			birthday: '',
+			password: '',
+			passwordConfirm: ''
+		}
+	}
+
+	onFormInputChange = (e) =>{
+		this.setState({[e.target.name]: e.target.value});
+	}
+
+	onFormSubmit = (e) =>{
+		e.preventDefault();
+		const user = {
+			username: this.state.username,
+			email: this.state.email,
+			location: this.state.location,
+			birthday: this.state.birthday,
+			password: this.state.password,
+			passwordConfirm: this.state.passwordConfirm
+		};
+
+		console.log(user);
+	}
 
 	render() {
 		return (
 			<div role="tabpanel" className="tab-pane" id="signup">
-	    	<form action="login.html" className="form">
+	    	<form onSubmit={this.onFormSubmit} className="form">
 	        <div className="form-group">
 	          <label>Username <small>(required)</small></label>
-	          <input type="text" className="form-control" placeholder="Enter Username"/>
+	          <input 
+	          	type="text" 
+	          	name="username"
+	          	onChange={this.onFormInputChange}
+	          	className="form-control"
+	          	placeholder="Enter Username" 
+	          	value={this.state.username}
+	          />
 	        </div>
 
 	        <div className="form-group">
 	          <label>Email <small>(required)</small></label>
-	          <input type="email" className="form-control" placeholder="Enter Email"/>
+	          <input 
+	          	type="email"
+	          	name="email"
+	          	onChange={this.onFormInputChange}
+	          	className="form-control" 
+	          	value={this.state.email}
+	          	placeholder="Enter Email"
+	          />
 	        </div>
 
 	        <div className="form-group">
 	          <label>Location <small>(required)</small></label>
-	          <input type="text" className="form-control" placeholder="Enter Location"/>
+	          <input
+	          	type="text"
+	          	name="location"
+	          	onChange={this.onFormInputChange}
+	          	className="form-control"
+	          	value={this.state.location}
+	          	placeholder="Enter Location"
+	          />
 	        </div>
 
 	        <div className="form-group">
 	          <label>Birthday <small>(required)</small></label>
-	          <input type="date" className="form-control" placeholder="Enter D.o.B"/>
+	          <input 
+	          	type="date" 
+	          	name="birthday"
+	          	onChange={this.onFormInputChange}
+	          	className="form-control" 
+	          	placeholder="Enter D.o.B"
+	          	value={this.state.birthday}
+	          />
 	        </div>
 
 	        <div className="form-group">
 	          <label>Password <small>(required)</small></label>
-	          <input type="password" className="form-control" placeholder="Password"/>
+	          <input 
+	          	type="password" 
+	          	name="password"
+	          	onChange={this.onFormInputChange}
+	          	placeholder="Password"
+	          	className="form-control" 
+	          	value={this.state.password}
+	          />
 	        </div>
+
+	        <div className="form-group">
+	          <label>Confirm Password<small>(required)</small></label>
+	          <input 
+	          	type="password" 
+	          	name="passwordConfirm"
+	          	onChange={this.onFormInputChange}
+	          	placeholder="Password Confirmation"
+	          	className="form-control" 
+	          	value={this.state.passwordConfirm}
+	          />
+	        </div>
+
 					<br/>
 	        <input type="submit" value="Signup" className="btn btn-danger btn-block"/>
 					<br/>
-	        <a href="#">Forgot Password?</a>
 	      </form>
+	      <a href="#">Forgot Password?</a>
 	    </div>
 		);
 	}
