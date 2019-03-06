4f6ca3d6d2cc44148bcf67b315e4ea349248761f
c0d1ngd0j0 <sam.gdouglas@gmail.com>
2019-03-06 06:40:28 -0500

    created auth reducer for redux store

--- client/src/ReduxStore/index.js
+++ client/src/ReduxStore/index.js
@@ -6,6 +6,12 @@ import rootReducer from "../Reducers/";
 const middleware = [thunk];
 const initialState = {};
 
-const store = createStore(() => rootReducer, initialState, applyMiddleware(...middleware));
+const store = createStore(
+	rootReducer, 
+	initialState, 
+	compose (applyMiddleware(...middleware), 
+		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+	)
+);
 
 export default store;
