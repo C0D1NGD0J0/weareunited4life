561cd1efe59f5fd4c8bddae0c6cb990d7b75dde2
c0d1ngd0j0 <sam.gdouglas@gmail.com>
2019-04-11 13:38:27 -0400

    seed data ready for deploymeny env

--- app/Database/seed.js
+++ app/Database/seed.js
@@ -6,35 +6,35 @@ const isProduction = (process.env.NODE_ENV === "production");
 const articlesData = require("./seed.json");
 
 if(isProduction){
-	categoryArr.forEach((category) =>{
-		Category.create({name: category});
-	});
+	// categoryArr.forEach((category) =>{
+	// 	Category.create({name: category});
+	// });
 
 	const seedData = Object.values(articlesData)[0];
 
-	// Category.find({}, {'_id': 1}).then((categories) =>{
-	// 	User.find({}, {'_id': 1}).limit(3).then((users) =>{
-	// 		seedData.slice(0,100).map((obj) =>{
-	// 			if(obj['language'] === 'english'){
-	// 				Post.create({
-	// 					body: truncateText(obj.text),
-	// 					title: truncateText(obj.title, 90),
-	// 					author: users[Math.floor(Math.random() * Math.floor(users.length))],
-	// 					allowComments: false,
-	// 					isMatch: false,
-	// 					photos: {
-	// 						location: obj["thread"].main_image,
-	// 						filename: "placeholder_image",
-	// 						size: 0
-	// 					},
-	// 					source: obj.url,
-	// 					postType: "article",
-	// 					category: categories[Math.floor(Math.random() * Math.floor(categories.length))]
-	// 				});
-	// 			}
-	// 		});
-	// 	});
-	// });
+	Category.find({}, {'_id': 1}).then((categories) =>{
+		User.find({}, {'_id': 1}).limit(3).then((users) =>{
+			seedData.slice(0,100).map((obj) =>{
+				if(obj['language'] === 'english'){
+					Post.create({
+						body: truncateText(obj.text),
+						title: truncateText(obj.title, 90),
+						author: users[Math.floor(Math.random() * Math.floor(users.length))],
+						allowComments: false,
+						isMatch: false,
+						photos: {
+							location: obj["thread"].main_image,
+							filename: "placeholder_image",
+							size: 0
+						},
+						source: obj.url,
+						postType: "article",
+						category: categories[Math.floor(Math.random() * Math.floor(categories.length))]
+					});
+				}
+			});
+		});
+	});
 	
 	const truncateText = (str, length = 1000) =>{
 		const defaultLength = length;
