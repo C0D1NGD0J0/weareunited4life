339bad312adb87d318e51a03bdc6041a9fdfef17
c0d1ngd0j0 <sam.gdouglas@gmail.com>
2019-04-11 15:56:41 -0400

    fixed errors

--- client/src/Actions/postAction.js
+++ client/src/Actions/postAction.js
@@ -10,6 +10,7 @@ export const getAllPostsAction = () => (dispatch) =>{
 			payload: res.data
 		});
 	}).catch((err) =>{
+		console.log("=======errror", err)
 		return dispatch({
 			type: GET_ERRORS,
 			payload: err.response.data
