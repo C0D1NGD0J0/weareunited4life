e94cfd4c45f2d0f98eb29da8b95db896e48dc093
c0d1ngd0j0 <sam.gdouglas@gmail.com>
2019-04-12 07:21:48 -0400

    production debugging...

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const socketIO = require("socket.io");
 const cors = require('cors');
 const passport = require("passport");
 const bodyParser = require('body-parser');
-const PORT = (process.env.PORT || 5000);
+const port = process.env.PORT || 5000;
 const app = express();
 const path = require("path");
 const corsOptions = {
@@ -26,16 +26,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(passport.initialize());
 
-// Serve static assets if in production env
-if(process.env.NODE_ENV === 'production'){
-	// Serve static files from the React app
-	app.use(express.static(path.join(__dirname, 'client/build')));
-
-	app.get("*", (req, res, next) =>{
-  	res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-	});
-};
-
 // Passport JWT Config
 passportConfig(passport);
 
@@ -54,6 +44,16 @@ app.use('/api/users', require('./app/Routes/user'));
 app.use('/api/posts', require('./app/Routes/post'));
 app.use('/api/', require('./app/Routes/comment'));
 app.use('/api/categories', require('./app/Routes/category'));
+
+// Serve static assets if in production env
+if(process.env.NODE_ENV === 'production'){
+	// Serve static files from the React app
+	app.use(express.static(path.join(__dirname, 'client/build')));
+
+	app.get("*", (req, res, next) =>{
+  	res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+	});
+};
 
 /*************************************** */
 // Socket logic starts here	
@@ -81,6 +81,6 @@ io.on("connection", (socket) =>{
 });
 
 // Initialize Server
-server.listen(PORT, (err) =>{
+server.listen(port, (err) =>{
 	console.log(`Server is live on port ${PORT}`);
 });
